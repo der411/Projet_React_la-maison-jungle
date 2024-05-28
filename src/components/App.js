28d00269c346329fa7d836d5db00d9ee6c99901f
der411 <vaitilingomvincent@gmail.com>
2024-05-28 19:16:45 +0200

    function ShoppingList() "reduce(), includes(), concat(), map()"

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner'
 import Cart from './Cart'
+import ShoppingList from './ShoppingList';
 
 function App() {
     return(
     <div>
       <Banner/> 
       <Cart/>
+      <ShoppingList/>
     </div>)
     
 }
