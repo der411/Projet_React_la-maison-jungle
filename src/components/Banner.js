abaaecd33e1a218d117600772aa7f489f32eafe0
der411 <vaitilingomvincent@gmail.com>
2024-05-29 18:17:56 +0200

    Listes et conditions

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,19 @@
-import logo from '../assets/logo.png'
-import '../styles/Banner.css'
+import logo from "../assets/logo.png";
+import "../styles/Banner.css";
+import Recommendation from "./Recommendations";
 
 function Banner() {
-    const title = 'La maison jungle'
-    return (
-        <div className='lmj-banner'>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1 className='lmj-title'>{title}</h1>
-        </div>
-    )
+  const title = "La maison jungle";
+  return (
+    
+    <div className='lmj-banner'>
+      <div className='lmj-banner-row'>
+        <img src={logo} alt="La maison jungle" className='lmj-logo' />
+        <h1 className='lmj-title'>{title}</h1>
+      </div>
+      <Recommendation />
+    </div>
+  )
 }
 
-export default Banner
+export default Banner;
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -2,46 +2,65 @@ export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
-		id: '1ed'
+		id: '1ed',
+		isBestSale: true,
+		isSpecialOffer: true
+
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
-		id: '2ab'
+		id: '2ab',
+		isBestSale: true,
+		isSpecialOffer: false
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
-		id: '3sd'
+		id: '3sd',
+		isBestSale: false,
+		isSpecialOffer: true
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
-		id: '4kk'
+		id: '4kk',
+		isBestSale: true,
+		isSpecialOffer: true
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
-		id: '5pl'
+		id: '5pl',
+		isBestSale: false,
+		isSpecialOffer: false
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
-		id: '6uo'
+		id: '6uo',
+		isBestSale: true,
+		isSpecialOffer: true
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
-		id: '7ie'
+		id: '7ie',
+		isBestSale: false,
+		isSpecialOffer: false
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
-		id: '8fp'
+		id: '8fp',
+		isBestSale: true,
+		isSpecialOffer: true
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn'
+		id: '9vn',
+		isBestSale: false,
+		isSpecialOffer: false
 	}
 ]
