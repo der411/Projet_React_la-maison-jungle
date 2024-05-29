00658f1817f40d30c2225c0be996577ec641888f
der411 <vaitilingomvincent@gmail.com>
2024-05-29 20:24:01 +0200

    Réutilisation du composant CareScale  'light' & 'water'

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -4,63 +4,80 @@ export const plantList = [
 		category: 'classique',
 		id: '1ed',
 		isBestSale: true,
-		isSpecialOffer: true
-
+		isSpecialOffer: true,
+		water:2,
+		light:3
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
 		isBestSale: true,
-		isSpecialOffer: false
+		isSpecialOffer: false,
+		water:3,
+		light:2
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
 		isBestSale: false,
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		water:1,
+		light:2
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
 		isBestSale: true,
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		water:2,
+		light:2
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
 		isBestSale: false,
-		isSpecialOffer: false
+		isSpecialOffer: false,
+		water:3,
+		light:3
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
 		id: '6uo',
 		isBestSale: true,
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		water:1,
+		light:3
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
 		isBestSale: false,
-		isSpecialOffer: false
+		isSpecialOffer: false,
+		water:2,
+		light:1
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
 		isBestSale: true,
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		water:3,
+		light:2
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
 		isBestSale: false,
-		isSpecialOffer: false
+		isSpecialOffer: false,
+		water: 1,
+		light:2
 	}
 ]
