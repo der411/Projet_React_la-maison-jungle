b1e2e4d9e9892b3c5ae695cf21977ea71d1bf3a2
der411 <vaitilingomvincent@gmail.com>
2024-05-30 15:02:25 +0200

    State local avec useState

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,35 +1,29 @@
-import { plantList } from '../datas/plantList'
-import PlantItem from './PlantItem'
-import '../styles/ShoppingList.css'
-
-function ShoppingList() {
-	const categories = plantList.reduce(
-		(acc, plant) =>
-			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-		[]
-	)
-
-	return (
-		<div>
-			<ul>
-				{categories.map((cat) => (
+function ShoppingList({ cart, updateCart }) {
+	// Petite précision : categories nous vient de la partie précédente pour récupérer toutes les catégories uniques de plantes.
+	
+		const categories = plantList.reduce(
+			(acc, elem) =>
+				acc.includes(elem.category) ? acc : acc.concat(elem.category),
+				[]
+		)
+		
+		return (
+			<div className='lmj-shopping-list'>
+				<ul>
+					{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
-				))}
-			</ul>
-			<ul className='lmj-plant-list'>
-				{plantList.map(({ id, cover, name, water, light }) => (
-					<PlantItem
-						key={id} 
-						id={id}
-						cover={cover}
-						name={name}
-						water={water}
-						light={light}
-					/>
-				))}
-			</ul>
-		</div>
-	)
-}
-
-export default ShoppingList
+					))}
+				</ul>
+				<ul className='lmj-plant-list'>
+					{plantList.map(({ id, cover, name, water, light }) => (
+						<div key={id}>
+							<PlantItem cover={cover} name={name} water={water} light={light} />
+							<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+						</div>
+					))}
+				</ul>
+			</div>
+		)
+	}
+	
+	export default ShoppingList
